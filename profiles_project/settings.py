2f78c5f3885c9221045193f1d1bb3d633e4c2c73
Skhre Khare <shrey199325@gmail.com>
2019-10-12 03:44:29 +0530

    COMMIT-7: Adding settings for deployment

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_rt33eddg3sktl1o5_-3xj8&$r$b*)lg=81c*3h#7#=(z^kgsn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get("DEBUG", 1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = "profiles_api.UserProfile"
+
+STATIC_ROOT = "static/"
